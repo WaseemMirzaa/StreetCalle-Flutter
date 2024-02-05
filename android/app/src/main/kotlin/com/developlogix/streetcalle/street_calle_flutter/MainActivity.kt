7d635908b7033968876eaa6a391a33342d7ccf8e
= <waseem@cognitivehealthintl.com>
2024-02-06 04:07:09 +0500

    ios folder updated

---
+++ android/app/src/main/kotlin/com/developlogix/streetcalle/street_calle_flutter/MainActivity.kt
@@ -0,0 +1,6 @@
+package com.developlogix.streetcalle.street_calle_flutter
+
+import io.flutter.embedding.android.FlutterActivity
+
+class MainActivity: FlutterActivity() {
+}
--- macos/Flutter/GeneratedPluginRegistrant.swift
+++ macos/Flutter/GeneratedPluginRegistrant.swift
@@ -12,7 +12,6 @@ import firebase_auth
 import firebase_core
 import firebase_storage
 import geolocator_apple
-import google_sign_in_ios
 import location
 import nb_utils
 import path_provider_foundation
@@ -28,7 +27,6 @@ func RegisterGeneratedPlugins(registry: FlutterPluginRegistry) {
   FLTFirebaseCorePlugin.register(with: registry.registrar(forPlugin: "FLTFirebaseCorePlugin"))
   FLTFirebaseStoragePlugin.register(with: registry.registrar(forPlugin: "FLTFirebaseStoragePlugin"))
   GeolocatorPlugin.register(with: registry.registrar(forPlugin: "GeolocatorPlugin"))
-  FLTGoogleSignInPlugin.register(with: registry.registrar(forPlugin: "FLTGoogleSignInPlugin"))
   LocationPlugin.register(with: registry.registrar(forPlugin: "LocationPlugin"))
   NbUtilsPlugin.register(with: registry.registrar(forPlugin: "NbUtilsPlugin"))
   PathProviderPlugin.register(with: registry.registrar(forPlugin: "PathProviderPlugin"))
